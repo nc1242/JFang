1c1b22599e8cc0e8955407b904a67f0c2937c30e
Noah Cheng <noahcheng@uslc02rnp31fvh3.enterprisenet.org>
2017-05-30 11:52:12 -0400

    first commit

---
+++ print.py
@@ -0,0 +1,12 @@
+import sys
+import sys
+import fileinput
+import re
+import time
+
+def main():
+    fileToEdit = sys.argv[1]
+    with open(fileToEdit 
+    
+if __name__ == "__main__":
+    main()
